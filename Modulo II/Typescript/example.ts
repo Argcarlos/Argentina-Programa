eb40c7e34b91d27d0a876859ec14496787676089
Carlos Quevedo <qcarlosadrian@gmail.com>
2021-12-06 21:43:21 -0300

    Last  commit

---
+++ Modulo II/Typescript/example.ts
@@ -0,0 +1,12 @@
+
+let age: number; 
+let c = 42;
+let d = "one";
+let computer: ['memory', 'cpu', 'motherboard', 'graphic card'];
+var a:number = 9;
+a += 4;
+
+function show(b:string) :void{
+  console.log(b);
+}
+show('hello');
