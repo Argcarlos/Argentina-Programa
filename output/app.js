a7655e6518a2cabdc62c8a7f4715f731be15c828
carlos <liveuser@pop-os.localdomain>
2021-12-06 22:20:29 -0300

    Typescript exercises

---
+++ output/app.js
@@ -0,0 +1,13 @@
+"use strict";
+let age;
+let c = 42;
+let d = "one";
+let computer;
+var a = 9;
+a += 4;
+function show(b) {
+    console.log(b);
+}
+show('hello');
+let message = 'Hello world';
+console.log(message);
